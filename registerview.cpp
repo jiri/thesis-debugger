cf8d21a7678709bf5f2a569679de97c80b4d077a
Jiří Šebele <sindriava@gmail.com>
2018-04-30 02:37:27 +0200

    RegisterView fixes

--- registerview.cpp
+++ registerview.cpp
@@ -14,6 +14,9 @@ RegisterView::RegisterView(QWidget* parent)
     this->verticalHeader()->setSectionResizeMode(QHeaderView::Fixed);
     this->verticalHeader()->setDefaultSectionSize(20);
 
+    this->horizontalHeader()->setDefaultSectionSize(48);
+    this->horizontalHeader()->setStretchLastSection(true);
+
     this->setMaximumWidth(120);
 }
 
